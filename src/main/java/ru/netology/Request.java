4171dd15025f2fc054935865e75ad2a404a60807
“Yuri <yura.kopshev@gmail.com>
2021-08-25 10:39:10 +0400

    changed server.java,main.java,added default-get.html

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,36 +1,36 @@
 package ru.netology;
 
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Request {
-
-    public  Map<String, String> parseQuerystring(String queryString) {
-        Map<String, String> map = new HashMap<String, String>();
+    public Map<String, String> parseQuerystring(String queryString) {
+        Map<String, String> map = new HashMap<>();
         if ((queryString == null) || (queryString.equals(""))) {
             return map;
         }
         String[] params = queryString.split("&");
         for (String param : params) {
-            try {
-                String[] keyValuePair = param.split("=", 2);
-                String name = URLDecoder.decode(keyValuePair[0], "UTF-8");
-                if (name == "") {
-                    continue;
-                }
-                String value = keyValuePair.length > 1 ? URLDecoder.decode(
-                        keyValuePair[1], "UTF-8") : "";
-                map.put(name, value);
-            } catch (UnsupportedEncodingException e) {
-                // ignore this parameter if it can't be decoded
+            String[] keyValuePair = param.split("=", 2);
+            String name = URLDecoder.decode(keyValuePair[0], StandardCharsets.UTF_8);
+            if (Objects.equals(name, "")) {
+                continue;
             }
+            String value = keyValuePair.length > 1 ? URLDecoder.decode(
+                    keyValuePair[1], StandardCharsets.UTF_8) : "";
+            map.put(name, value);
         }
         return map;
     }
 
-    public String queryString(String uri){
-        return uri.split("\\?")[1];
+    public String queryString(String uri) throws MalformedURLException {
+        return uri.substring(2);
     }
 }
+
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,23 +1,29 @@
 package ru.netology;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import static java.util.List.*;
+
+import java.io.IOException;
+
 
 public class Server implements Runnable {
-
+    public static final String GET = "GET";
+    public static final String POST = "POST";
     private final Socket socket;
+    final List<String> allowedMethods = of(GET, POST);
+    final int limit = 4096;
+    Request request = new Request();
 
     public Server(Socket socket) {
         this.socket = socket;
     }
 
-    final List<String> validPaths = List.of("/index.html", "/spring.svg",
+    final List<String> validPaths = of("/index.html", "/spring.svg",
             "/spring.png", "/resources.html", "/styles.css", "/app.js",
             "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
@@ -25,49 +31,131 @@ public class Server implements Runnable {
     @Override
     public void run() {
         try {
-            final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            final var in = new BufferedInputStream(socket.getInputStream());
             final var out = new BufferedOutputStream(socket.getOutputStream());
 
-            // read only request line for simplicity
-            // must be in form GET /path HTTP/1.1
-            final var requestLine = in.readLine();
-            final var parts = requestLine.split(" ");
+            // лимит на request line + заголовки
+            in.mark(limit);
+            byte[] buffer = new byte[limit];
+            int read = in.read(buffer);
 
-            if (parts.length != 3) {
-                // just close socket
-                return;
+            // ищем request line
+            final var requestLineDelimiter = new byte[]{'\r', '\n'};
+            final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+            if (requestLineEnd == -1) {
+                badRequest(out);
+
             }
 
-            final var path = parts[1];
-            if (!validPaths.contains(path)) {
-                out.write((
-                        "HTTP/1.1 404 Not Found\r\n" +
-                                "Content-Length: 0\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.flush();
+            // читаем request line
+            final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+            if (requestLine.length != 3) {
+                badRequest(out);
                 return;
+
             }
 
-            final var filePath = Path.of(".", "public", path);
-            final var mimeType = Files.probeContentType(filePath);
+            final var method = requestLine[0];
+            if (!allowedMethods.contains(method)) {
+                badRequest(out);
+                //415
+                return;
+            }
+            System.out.println(method);
 
+            final var path = requestLine[1];
+            if (!path.startsWith("/")) {
+                badRequest(out);
+                return;
+            }
+            System.out.println(path);
+            System.out.println("Query параметры в виде map:");
+            //
+            String queryString = request.queryString(path);
+            System.out.println(request.parseQuerystring(queryString));
 
-            final var length = Files.size(filePath);
+            // ищем заголовки
+            final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+            final var headersStart = requestLineEnd + requestLineDelimiter.length;
+            final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+            if (headersEnd == -1) {
+                badRequest(out);
+            }
+
+            // отматываем на начало буфера
+            in.reset();
+            // пропускаем requestLine
+            in.skip(headersStart);
+
+            final var headersBytes = in.readNBytes(headersEnd - headersStart);
+            final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+            System.out.println(headers);
+
+            // для GET тела нет
+            if (!method.equals(GET)) {
+                in.skip(headersDelimiter.length);
+                // вычитываем Content-Length, чтобы прочитать body
+                final var contentLength = extractHeader(headers, "Content-Length");
+                if (contentLength.isPresent()) {
+                    final var length = Integer.parseInt(contentLength.get());
+                    final var bodyBytes = in.readNBytes(length);
+
+                    final var body = new String(bodyBytes);
+                    System.out.println(body);
+                }
+            }
+
             out.write((
                     "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
+                            "Content-Length: 0\r\n" +
                             "Connection: close\r\n" +
                             "\r\n"
             ).getBytes());
-            Files.copy(filePath, out);
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+
+    private static Optional<String> extractHeader(List<String> headers, String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    private static void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+    // from google guava with modifications
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+
+    public List<String> getValidPaths() {
+        return validPaths;
     }
 }
+
 
 
